c0d0d015ef016ad2427a0564c3a6d1225f1cac0b
Aidan Ih <aidan.h.ih@gmail.com>
2024-05-13 15:12:01 -0400

    update events summer

--- src/Site/FeaturedEvents/featured.tsx
+++ src/Site/FeaturedEvents/featured.tsx
@@ -5,28 +5,20 @@ interface featuredEvent {
 
 var featured : featuredEvent[] = [
     {
-        "name": "Prodigy Smash Monthly",
-        "id": 641071
-    },
-    {
-        "name": "The Safari",
-        "id": 638077
-    },
-    {
-        "name": "Mecha Noodle Bar: Boston",
-        "id": 645560
-    },
-    {
         "name": "Boston Blue Beat: Beach Episode",
         "id": 612088
     },
     {
-        "name": "Granite State Grind",
-        "id": 644640
+        "name": "Prodigy Classic",
+        "id": 657826
     },
     {
-        "name": "The Bakery",
-        "id": 643945
+        "name": "Mash Harder 9",
+        "id": 656606
+    },
+    {
+        "name": "Critical Mass",
+        "id": 639392
     }
 ]
 
