22c3d70b79f0a21f2a3e827b121cf01e83154081
Aidan Ih <aidan.h.ih@gmail.com>
2025-03-09 23:55:15 -0400

    update featured

--- src/Site/FeaturedEvents/featured.tsx
+++ src/Site/FeaturedEvents/featured.tsx
@@ -9,9 +9,22 @@ var featured : featuredEvent[] = [
         "id": 732885
     },
     {
+        'name': 'CT Gamercon 2025',
+        'id':748635
+    },
+    {
+        'name': 'Prodigy Classic IV',
+        'id': 753470
+    },
+    {
         'name': 'New England Arcadian',
         'id': 754964
+    },
+    {
+        'name': 'Waterville Smash Attack XX',
+        'id': 751426
     }
+    
 ]
 
 export default featured;
